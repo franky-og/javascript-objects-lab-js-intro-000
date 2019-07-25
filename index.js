434fda682136cdaeedf8f55431c4281f27d33737
Franky Liu <cuifranky95@gmail.com>
2019-07-25 02:23:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var recipes = {
   food: 'ingredients'
 }
 function updateObjectWithKeyAndValue(recipes, food, ingredients) {
-  recipes[food] = ingredients;
-  return recipes;
+  var newRecipe = 
   
 }
